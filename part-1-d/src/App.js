a741d1c75c82aff4a1c6c9ab810283688cb255f4
Aisha-R <aish0695@stud.kea.dk>
2022-10-08 16:02:03 +0200

    1.10: Unicafe Step 5

--- part-1-d/src/App.js
+++ part-1-d/src/App.js
@@ -2,38 +2,38 @@ import { useState } from 'react'
 
 const Header = ({ text }) => <h2>{text}</h2>
 
-const Button = ({text, handleClick}) => <button onClick={handleClick}>{text}</button>
+const Button = ({ text, handleClick }) => <button onClick={ handleClick }>{ text }</button>
 
 const Buttons = ({ text, handleClick }) => {
     return (
         <div>
-            <Button text={text[0]} handleClick={handleClick[0]} />
-            <Button text={text[1]} handleClick={handleClick[1]} />
-            <Button text={text[2]} handleClick={handleClick[2]} />
+            <Button text={ text[0] } handleClick={ handleClick[0] } />
+            <Button text={ text[1] } handleClick={ handleClick[1] } />
+            <Button text={ text[2] } handleClick={ handleClick[2] } />
         </div>
     )
 }
 
-const Feedback = ({text, result}) => <p>{text} {result}</p>
+const Feedback = ({ text, result }) => <p>{ text } { result }</p>
 
 const Feedbacks = ({ text, result }) => {
     return (
         <div>
-            <Feedback text={text[0]} result={result[0]} />
-            <Feedback text={text[1]} result={result[1]} />
-            <Feedback text={text[2]} result={result[2]} />
+            <Feedback text={ text[0] } result={ result[0] } />
+            <Feedback text={ text[1] } result={ result[1] } />
+            <Feedback text={ text[2] } result={ result[2] } />
         </div>
     )
 }
 
-const Statistic = ({ measure, calculation }) => <p>{measure} {calculation}</p>
+const StatisticLine = ({ measure, calculation }) => <p>{ measure } { calculation }</p>
 
 const Statistics = ({ measure, calculation }) => {
     return (
         <div>
-            <Statistic measure={measure[0]} calculation={calculation[0]} />
-            <Statistic measure={measure[1]} calculation={calculation[1]} />
-            <Statistic measure={measure[2]} calculation={calculation[2]} />
+            <StatisticLine measure={ measure[0] } calculation={ calculation[0] } />
+            <StatisticLine measure={ measure[1] } calculation={ calculation[1] } />
+            <StatisticLine measure={ measure[2] } calculation={ calculation[2] } />
         </div>
     )
 }
@@ -50,8 +50,8 @@ const History = ({ text, result, measure, calculation }) => {
     
     return (
         <div>
-            <Feedbacks text={text} result={result} />
-            <Statistics measure={measure} calculation={calculation} />
+            <Feedbacks text={ text } result={ result } />
+            <Statistics measure={ measure } calculation={ calculation } />
         </div>
     )
 }
@@ -73,9 +73,9 @@ const App = () => {
     return (
         <div>
             <Header text="give feedback" />
-            <Buttons text={[goodText, neutralText, badText]} handleClick={[() => setGood(good + 1), () => setNeutral(neutral + 1), () => setBad(bad + 1)]}/>
+            <Buttons text={ [goodText, neutralText, badText] } handleClick={ [() => setGood(good + 1), () => setNeutral(neutral + 1), () => setBad(bad + 1)] }/>
             <Header text="statistics" />
-            <History text={[goodText, neutralText, badText]} result={[good, neutral, bad]} measure={["all", "average", "positive"]} calculation={[all(), average(), positive()]} />
+            <History text={ [goodText, neutralText, badText] } result={ [good, neutral, bad] } measure={ ["all", "average", "positive"] } calculation={ [all(), average(), positive()] } />
         </div>
     )
 }
