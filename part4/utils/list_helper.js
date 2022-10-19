7468d4a5afc3c10a76162f109c25cdcb1635e0ee
Aisha-R <aish0695@stud.kea.dk>
2022-10-19 10:56:19 +0200

    4.5*: Helper Functions & Unit Tests - Step 3

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -9,7 +9,26 @@ const totalLikes = (blogs) => {
     return blogs.reduce(sum, 0)
 }
 
+const favouriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return null
+    }
+
+    const upvotes = blogs.map(blog => {
+        return blog.likes
+    })
+
+    const maxLikes = Math.max(...upvotes)
+
+    const blog = blogs.find(blog => blog.likes === maxLikes)
+
+    const { title, author, likes } = blog
+
+    return { title, author, likes }
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favouriteBlog
 }
