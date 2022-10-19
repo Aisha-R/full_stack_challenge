41e301a28a8b2405faea4d7e02f7ba43332920aa
Aisha-R <aish0695@stud.kea.dk>
2022-10-19 12:06:16 +0200

    4.6*: Helper Functions & Unit Tests - Step 4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -27,8 +27,51 @@ const favouriteBlog = (blogs) => {
     return { title, author, likes }
 }
 
+const listWithOneBlog = [
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+    }
+]
+
+const mostBlogs = (blogs) => {
+
+    if (blogs.length === 0) {
+        return null
+    }
+
+    const authors = blogs.map(blog => blog.author)
+    const uniqueAuthors = [...new Set(authors)];
+    const count = []
+
+    uniqueAuthors.forEach(author => count.push({author: author, blogs: 0}))
+
+    authors.forEach(author => {
+        count.forEach(obj => {
+            if (obj.author === author) {
+                obj.blogs += 1
+            }
+        })
+    })
+
+    const noOfBlogs = count.map(obj => {
+        return obj.blogs
+    })
+
+    const maxBlogs = Math.max(...noOfBlogs)
+
+    const author = count.find(obj => obj.blogs === maxBlogs)
+
+    return author
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favouriteBlog
+    favouriteBlog,
+    mostBlogs
 }
