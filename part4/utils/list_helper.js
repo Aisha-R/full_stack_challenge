5fa7504c511ca516f7f77572a41924d66182010e
Aisha-R <aish0695@stud.kea.dk>
2022-10-19 10:33:48 +0200

    4.4: Helper Functions & Unit Tests - Step 2

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const sum = (accumulator, blogs) => {
+        return accumulator + blogs.likes
+    }
+    return blogs.reduce(sum, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
