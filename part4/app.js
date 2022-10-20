1b20e842b0a0037597453df08b93205de0984418
Aisha-R <aish0695@stud.kea.dk>
2022-10-20 11:36:01 +0200

    4.18: Blog List Expansion - Step 6

--- part4/app.js
+++ part4/app.js
@@ -1,10 +1,12 @@
 const { MONGODB_URI, PORT } = require('./utils/config')
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 const blogsRouter = require('./controllers/blogs')
 const cors = require('cors')
+const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
 mongoose.connect(MONGODB_URI)
@@ -15,5 +17,8 @@ app.use(express.json())
 app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -11,6 +20,13 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
     const blog = new Blog(request.body)
 
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+
     if (!blog.title || !blog.url) {
         return response.status(400).end()
     }
@@ -18,8 +34,6 @@ blogsRouter.post('/', async (request, response) => {
     if (!blog.likes) {
         blog.likes = 0
     }
-
-    const user = await User.findOne()
 
     blog.user = user._id
 
