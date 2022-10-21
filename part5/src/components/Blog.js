6b06222794bb0fa508753189ed03f61cc50a8c86
Aisha-R <aish0695@stud.kea.dk>
2022-10-21 19:55:26 +0200

    5.7*: Blog List Frontend - Step 7

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,7 +1,27 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import Togglable from './Togglable'
+
+const Blog = ({ blog }) => {
+
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5
+    }
+
+    const pStyle = { margin: 5 }
+
+    return (
+        <div style={blogStyle}>
+            <p style={pStyle}>{blog.title} {blog.author}</p>
+            <Togglable buttonLabel='view'>
+                <p style={pStyle}>{blog.url}</p>
+                <p style={pStyle}>likes {blog.likes}</p> <button>like</button>
+                <p style={pStyle}>{blog.user.name}</p>
+            </Togglable>
+        </div>
+    )
+}
 
 export default Blog
