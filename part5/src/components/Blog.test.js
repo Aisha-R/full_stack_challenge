bc4cadc6275e447dc75dbba87c214bdf5d70b524
Aisha-R <aish0695@stud.kea.dk>
2022-10-22 20:15:27 +0200

    5.14: Blog List Tests - Step 2

--- part5/src/components/Blog.test.js
+++ part5/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
@@ -32,6 +33,16 @@ describe('<Blog />', () => {
 
         const toggleDiv = container.querySelector('.hiddenInitially')
         expect(toggleDiv).toHaveStyle('display: none')
+    })
+
+    test('after clicking button, children are displayed', async () => {
+
+        const user = userEvent.setup()
+        const button = screen.getByText('view')
+        await user.click(button)
+
+        const toggleDiv = container.querySelector('.hiddenInitially')
+        expect(toggleDiv).not.toHaveStyle('display: none')
     })
 
 })
