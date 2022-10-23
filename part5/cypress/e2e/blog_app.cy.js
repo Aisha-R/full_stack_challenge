55fbcd77c6eb158e89412ad75b214d3025f422fc
Aisha-R <aish0695@stud.kea.dk>
2022-10-23 11:48:32 +0200

    5.18: Blog List End-to-End Testing - Step 2

--- part5/cypress/e2e/blog_app.cy.js
+++ part5/cypress/e2e/blog_app.cy.js
@@ -2,10 +2,36 @@ describe('Blog app', function () {
 
     beforeEach(function () {
         cy.request('POST', 'http://localhost:3003/api/testing/reset')
+        const user = {
+            name: 'Aisha Rooble',
+            username: 'Aisha-R',
+            password: 'password123'
+        }
+        cy.request('POST', 'http://localhost:3003/api/users/', user)
         cy.visit('http://localhost:3000')
     })
 
     it('Login form is shown', function () {
         cy.contains('login')
+    })
+
+    describe('Login', function () {
+        it('succeeds with correct credentials', function () {
+            cy.contains('login').click()
+            cy.get('#username').type('Aisha-R')
+            cy.get('#password').type('password123')
+            cy.get('#login-button').click()
+
+            cy.contains('Aisha Rooble logged-in')
+        })
+
+        it('fails with wrong credentials', function () {
+            cy.contains('login').click()
+            cy.get('#username').type('Aisha-R')
+            cy.get('#password').type('wrong')
+            cy.get('#login-button').click()
+
+            cy.contains('invalid username or password')
+        })
     })
 })
