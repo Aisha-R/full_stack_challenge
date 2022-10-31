380bf47d93d9a0d25bb48e5ae8d14e4e60558a06
Aisha-R <aish0695@stud.kea.dk>
2022-10-31 17:50:02 +0100

    7.18: Comments - Step 1

--- part7/blog-list/src/components/Blog.js
+++ part7/blog-list/src/components/Blog.js
@@ -38,6 +38,12 @@ const Blog = ({ user, blogs, navigate, matchBlog }) => {
 					remove
 				</button>
 			}
+			<h3>comments</h3>
+			<ul>
+				{blog.comments.map((comment, index) =>
+					<li key={index}>{comment}</li>
+				)}
+			</ul>
 		</div>
 	)
 }
