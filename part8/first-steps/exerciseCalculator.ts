350e03a8d1d6b88944d1e7034f1b5e8cecde7e79
Aisha-R <aish0695@stud.kea.dk>
2022-11-04 22:30:48 +0100

    9.3: Command Line

--- part8/first-steps/exerciseCalculator.ts
+++ part8/first-steps/exerciseCalculator.ts
@@ -1,3 +1,37 @@
+interface ExerciseValues {
+    value1: number;
+    value2: Array<number>;
+}
+
+const parseArguments = (args: Array<string>): ExerciseValues => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+
+    const arr1 = args.slice(3);
+
+    const arr2 = arr1.map(entry => Number(entry));
+
+    if (!isNaN(Number(args[2]))) {
+        let notANumber = false;
+
+        arr2.forEach(entry => {
+            if (isNaN(entry)) {
+                notANumber = true
+            }
+        });
+
+        if (notANumber) {
+            throw new Error('Provided values were not numbers!');
+        } else {
+            return {
+                value1: Number(args[2]),
+                value2: arr2
+            }
+        }
+    } else {
+        throw new Error('Provided values were not numbers!');
+    }
+}
+
 interface Result {
     periodLength: number;
     trainingDays: number;
@@ -42,4 +76,13 @@ const calculateExercises = (actual: Array<number>, target: number): Result => {
     };
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+try {
+    const { value1, value2 } = parseArguments(process.argv);
+    console.log(calculateExercises(value2, value1));
+} catch (error: unknown) {
+    let errorMessage = 'Something bad happened.'
+    if (error instanceof Error) {
+        errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
+}
